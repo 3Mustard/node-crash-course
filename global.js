3dece903b7b1d8019f1a4022b2e4bdf22d37ee15
Matt <m23.cullen@gmail.com>
2020-06-16 00:35:30 -0400

    example of getting current directory and filepath

--- global.js
+++ global.js
@@ -3,15 +3,19 @@
 // console.log(global);
 
 
-global.setTimeout(() => {
-    console.log('timeout!');
-    clearInterval(int);
-}, 3000);
+// global.setTimeout(() => {
+//     console.log('timeout!');
+//     clearInterval(int);
+// }, 3000);
 
-setTimeout(() => {
-    console.log('global is implied in this timeout!')
-}, 3000);
+// setTimeout(() => {
+//     console.log('global is implied in this timeout!')
+// }, 3000);
 
-const int = setInterval(() => {
-    console.log('interval')
-}, 1000);
+// const int = setInterval(() => {
+//     console.log('interval')
+// }, 1000);
+
+//Logs the directory we are in and the current filepath
+console.log(__dirname);
+console.log(__filename);
