01cdf3f4ac5bb644ec97acc8c828fd980ec4cff0
Matt <m23.cullen@gmail.com>
2020-06-15 23:47:57 -0400

    example of setting and clearing a global interval

--- global.js
+++ global.js
@@ -5,8 +5,13 @@
 
 global.setTimeout(() => {
     console.log('timeout!');
+    clearInterval(int);
 }, 3000);
 
 setTimeout(() => {
     console.log('global is implied in this timeout!')
 }, 3000);
+
+const int = setInterval(() => {
+    console.log('interval')
+}, 1000);
