f9ae1bdf18128119fc10370bcc7ea2eaa51eb3e6
Matt <m23.cullen@gmail.com>
2020-06-15 23:44:11 -0400

    example of global being implied

--- global.js
+++ global.js
@@ -1,3 +1,12 @@
 // Global object
 
-console.log(global);
+// console.log(global);
+
+
+global.setTimeout(() => {
+    console.log('timeout!');
+}, 3000);
+
+setTimeout(() => {
+    console.log('global is implied in this timeout!')
+}, 3000);
