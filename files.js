8ae0123e7a830c691b37084226234605c8a26b5f
Matt <m23.cullen@gmail.com>
2020-06-17 21:15:34 -0400

    finised example of reading files

--- files.js
+++ files.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 
 // reading files, asyncronous, takes in filepath and a function to run after it gets the file
-fs.readFile('./docs/blog1.txt', (error, data) => {
-    if ( error ) {
-        console.log(error);
-    }
-    console.log(data.toString());
-});
+// fs.readFile('./docs/blog1.txt', (error, data) => {
+//     if ( error ) {
+//         console.log(error);
+//     }
+//     console.log(data.toString());
+// });
+// console.log('last line?');
 
 // writing files
-
+fs.writeFile('./docs/blog1.txt', )
 
 // directories
 
